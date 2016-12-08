10dd9dd242ad05f4440a3b6d31cb0dba21578dbc
Salva GM <salvagarciamendez@gmail.com>
2016-12-08 19:02:11 +0000

    Create ellipse class

---
+++ SpreadEX/DrawLib/Ellipse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadEX.DrawLib
+{
+    class Ellipse : BaseShape
+    {
+        public int DiameterH { get; set; }
+        public int DiameterV { get; set; }
+
+        public Ellipse() : base()
+        {
+            DiameterH = 0;
+            DiameterV = 0;
+        }
+
+        public Ellipse(int xPosition, int yPosition, int diameterH, int diameterV) : base(xPosition,yPosition)
+        {
+            DiameterH = diameterH;
+            DiameterV = diameterV;
+        }
+
+
+        public override void Print()
+        {
+            base.Print();
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}  diameterH = {1} diameterV = {2}", base.ToString(), DiameterH, DiameterV);
+        }
+    }
+}
--- SpreadEX/Program.cs
+++ SpreadEX/Program.cs
@@ -14,7 +14,7 @@ namespace SpreadEX
             // Exercise
             shapes.Add(new Square(xPosition: 15, yPosition: 30,size: 35));
             shapes.Add(new Rectangle(xPosition: 10, yPosition: 10, width: 30, height: 40));
-            //shapes.Add(new Ellipse(xPosition: 100, yPosition: 150, hDiameter: 300, vDiameter: 200));
+            shapes.Add(new Ellipse(xPosition: 100, yPosition: 150, diameterH : 300, diameterV: 200));
             shapes.Add(new Circle(xPosition: 1, yPosition: 1, size: 300));
             //shapes.Add(new Textbox(xPosition: 5, yPosition: 5, width: 200, height: 100, text: "Sample text"));
 
