3342a6f751b4c65fd702031208c64ebf657776b5
Salva GM <salvagarciamendez@gmail.com>
2016-12-08 17:46:19 +0000

    Create initial package

---
+++ SpreadEX/DrawLib/Coordinates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadEX.DrawLib
+{
+    class Coordinates
+    {
+        public float X
+        {
+            get; set;
+        }
+
+        public float Y
+        {
+            get; set;
+        }
+
+        public Coordinates
+    }
+}
